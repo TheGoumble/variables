0642e0c3230663118db7d583aea54b55756f5784
Javier Vargas <thebruh@Javiers-MacBook-Air.local>
2022-07-14 11:59:03 -0400

    intro to variables

---
+++ variables.js
@@ -0,0 +1,15 @@
+// first comment
+/* second commend 
+in multiple lines
+*/
+let myAge = 49;
+console.log(`my age is ${myAge} and I'm Canadian
+
+
+
+Eh!`);
+
+
+// console.log('My name is Damian');
+// console.log("My name is Damian");
+// console.log(`My name is Damian`);
